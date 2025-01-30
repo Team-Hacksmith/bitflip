4bc0d9e3a2b7fc59a98029377bd202e226630d11
adityanandanx <nandanaditya985@gmail.com>
2025-01-31 02:39:41 +0530

    feat: setup player, tilemap and background

---
+++ scripts/player.gd
@@ -0,0 +1,134 @@
+extends CharacterBody2D
+
+@export var speed = 300
+@export var gravity = 30
+@export var jump_force = 300
+
+@onready var ap = $AnimationPlayer
+@onready var sprite = $Sprite2D
+@onready var cshape = $CollisionShape2D
+@onready var crouch_raycast1 = $CrouchRaycast_1
+@onready var crouch_raycast2 = $CrouchRaycast_2
+@onready var coyote_timer = $CoyoteTimer
+@onready var jump_buffer_timer = $JumpBufferTimer
+@onready var jump_height_timer = $JumpHeightTimer
+
+var is_crouching = false
+var stuck_under_object = false
+var can_coyote_jump = false
+var jump_buffered = false
+
+func _physics_process(delta):
+	if !is_on_floor() && (can_coyote_jump == false):
+		velocity.y += gravity
+		if velocity.y > 1000:
+			velocity.y = 1000
+	
+	if Input.is_action_just_pressed("jump"):
+		jump_height_timer.start()
+		jump()
+	
+	var horizontal_direction = Input.get_axis("move_left", "move_right")
+	velocity.x = speed * horizontal_direction
+	
+	if horizontal_direction != 0:
+		switch_direction(horizontal_direction)
+	
+	if Input.is_action_just_pressed("crouch"):
+		crouch()
+	elif Input.is_action_just_released("crouch"):
+		if above_head_is_empty():
+			stand()
+		else:
+			if stuck_under_object != true:
+				stuck_under_object = true
+				print("Player stuck, setting stuck_under_object to true")
+	
+	if stuck_under_object && above_head_is_empty():
+		if !Input.is_action_pressed("crouch"):
+			stand()
+			stuck_under_object = false
+			print("Player was stuck but he is getting up")
+	
+	var was_on_floor = is_on_floor()
+	move_and_slide()
+	
+	# Started to fall
+	if was_on_floor && !is_on_floor() && velocity.y >= 0:
+		can_coyote_jump = true
+		coyote_timer.start()
+	
+	# Touched ground
+	if !was_on_floor && is_on_floor():
+		if jump_buffered:
+			jump_buffered = false
+			print("Buffered jump")
+			jump()
+	
+	update_animations(horizontal_direction)
+
+func jump():
+	if is_on_floor() || can_coyote_jump:
+		velocity.y = -jump_force
+		if can_coyote_jump:
+			can_coyote_jump = false
+			print("Coyote jump")
+	else:
+		if !jump_buffered:
+			jump_buffered = true
+			jump_buffer_timer.start()
+
+func _on_coyote_timer_timeout():
+	can_coyote_jump = false
+
+func _on_jump_buffer_timer_timeout():
+	jump_buffered = false
+
+func _on_jump_height_timer_timeout():
+	if !Input.is_action_pressed("jump"):
+		if velocity.y < -100:
+			velocity.y = -100
+			print("Low jump")
+	else:
+		print("High jump")
+
+func above_head_is_empty() -> bool:
+	var result = !crouch_raycast1.is_colliding() && !crouch_raycast2.is_colliding()
+	return result
+
+func update_animations(horizontal_direction):
+	if is_on_floor():
+		if horizontal_direction == 0:
+			if is_crouching:
+				ap.play("crouch")
+			else:
+				ap.play("idle")
+		else:
+			if is_crouching:
+				ap.play("crouch_walk")
+			else:
+				ap.play("run")
+	else:
+		if is_crouching == false:
+			if velocity.y < 0:
+				ap.play("jump")
+			elif velocity.y > 0:
+				ap.play("fall")
+		else:
+			ap.play("crouch")
+
+func switch_direction(horizontal_direction):
+	sprite.flip_h = (horizontal_direction == -1)
+	sprite.position.x = horizontal_direction * 4
+
+func crouch():
+	if is_crouching:
+		return
+	is_crouching = true
+	cshape.position.y = -12
+
+func stand():
+	if is_crouching == false:
+		return
+	is_crouching = false
+	cshape.position.y = -16
