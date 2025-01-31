c622b617efd09ae8ee825e332be6b654be92a91c
adityanandanx <nandanaditya985@gmail.com>
2025-02-01 00:17:35 +0530

    feat: player sprite

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,20 +1,16 @@
 extends CharacterBody2D
 
-@export var speed = 100
+@export var speed = 30
+@export var friction = 0.9
 @export var gravity = 10
 @export var jump_force = 300
 
 @onready var ap = $AnimationPlayer
 @onready var sprite = $Sprite2D
-@onready var cshape = $CollisionShape2D
-@onready var crouch_raycast1 = $CrouchRaycast_1
-@onready var crouch_raycast2 = $CrouchRaycast_2
 @onready var coyote_timer = $CoyoteTimer
 @onready var jump_buffer_timer = $JumpBufferTimer
 @onready var jump_height_timer = $JumpHeightTimer
 
-var is_crouching = false
-var stuck_under_object = false
 var can_coyote_jump = false
 var jump_buffered = false
 var is_disabled = false
@@ -32,6 +28,7 @@ func _physics_process(delta):
 	handle_inputs(horizontal_direction)
 	
 	var was_on_floor = is_on_floor()
+	velocity.x *= friction
 	move_and_slide()
 	
 	# Started to fall
@@ -54,26 +51,10 @@ func handle_inputs(horizontal_direction: float):
 		jump_height_timer.start()
 		jump()
 	
-	velocity.x = speed * horizontal_direction
+	velocity.x += speed * horizontal_direction
 	
 	if horizontal_direction != 0:
 		switch_direction(horizontal_direction)
-	
-	if Input.is_action_just_pressed("crouch"):
-		crouch()
-	elif Input.is_action_just_released("crouch"):
-		if above_head_is_empty():
-			stand()
-		else:
-			if stuck_under_object != true:
-				stuck_under_object = true
-				print("Player stuck, setting stuck_under_object to true")
-	
-	if stuck_under_object && above_head_is_empty():
-		if !Input.is_action_pressed("crouch"):
-			stand()
-			stuck_under_object = false
-			print("Player was stuck but he is getting up")
 
 func jump():
 	if is_on_floor() || can_coyote_jump:
@@ -100,43 +81,18 @@ func _on_jump_height_timer_timeout():
 	else:
 		print("High jump")
 
-func above_head_is_empty() -> bool:
-	var result = !crouch_raycast1.is_colliding() && !crouch_raycast2.is_colliding()
-	return result
-
 func update_animations(horizontal_direction):
 	if is_on_floor():
 		if horizontal_direction == 0:
-			if is_crouching:
-				ap.play("crouch")
-			else:
-				ap.play("idle")
+			ap.play("idle")
 		else:
-			if is_crouching:
-				ap.play("crouch_walk")
-			else:
-				ap.play("run")
+			ap.play("run")
 	else:
-		if is_crouching == false:
-			if velocity.y < 0:
-				ap.play("jump")
-			elif velocity.y > 0:
-				ap.play("fall")
-		else:
-			ap.play("crouch")
+		if velocity.y < 0:
+			ap.play("jump")
+		elif velocity.y > 0:
+			ap.play("fall")
 
 func switch_direction(horizontal_direction):
 	sprite.flip_h = (horizontal_direction == -1)
 	sprite.position.x = horizontal_direction * 4
-
-func crouch():
-	if is_crouching:
-		return
-	is_crouching = true
-	cshape.position.y = -12
-
-func stand():
-	if is_crouching == false:
-		return
-	is_crouching = false
-	cshape.position.y = -16
