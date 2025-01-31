d9a8301112e34c111d3a82f0673aaa9d6a7eb149
adityanandanx <nandanaditya985@gmail.com>
2025-01-31 15:35:34 +0530

    feat: ground tileset autotiling

--- dialogs/characters/portraits/animated_sprite_portrait.gd
+++ dialogs/characters/portraits/animated_sprite_portrait.gd
@@ -3,11 +3,17 @@ extends DialogicPortrait
 
 # If the custom portrait accepts a change, then accept it here
 func _update_portrait(passed_character: DialogicCharacter, passed_portrait: String) -> void:
-	if passed_portrait == "":
-		passed_portrait = passed_character['default_portrait']
+	apply_character_and_portrait(passed_character, passed_portrait)
 
 	if $Sprite.sprite_frames.has_animation(passed_portrait):
 		$Sprite.play(passed_portrait)
+		
+func _set_mirror(is_mirrored: bool) -> void:
+	var sprite: AnimatedSprite2D = $Sprite
+	if is_mirrored:
+		sprite.flip_h = 1
+	else:
+		sprite.flip_h = 0
 
 func _on_animation_finished() -> void:
 	$Sprite.frame = randi() % $Sprite.sprite_frames.get_frame_count($Sprite.animation)
