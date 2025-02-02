ee5284fc66bfad28697eb0b15b3154c1c528c289
adityanandanx <nandanaditya985@gmail.com>
2025-02-02 19:41:31 +0530

    feat: hidden spikes

--- scripts/dead_zone.gd
+++ scripts/dead_zone.gd
@@ -1,6 +1,20 @@
 extends Area2D
 
+@export var disabled: bool = false:
+	set(new):
+		disabled = new
+		if is_player_inside:
+			Global.player_stats.health = 0
+
+var is_player_inside = false
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		Global.player_stats.health = 0
+		if not disabled:
+			Global.player_stats.health = 0
+		is_player_inside = true
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		is_player_inside = false
---
+++ scripts/hidden_spikes.gd
@@ -0,0 +1,46 @@
+extends Node2D
+
+@export var time_based: bool = true
+@export var hidden_time = 4
+@export var active_time = 1
+@export var state: SpikeState = SpikeState.HIDDEN:
+	set(new):
+		if not sprite: return
+		state = new
+		_on_state_change(state)
+@onready var timer: Timer = $Timer
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+enum SpikeState {
+	HIDDEN,
+	ACTIVE,
+}
+
+
+func _ready() -> void:
+	_on_state_change(state)
+	if time_based:
+		timer.start()
+	else:
+		timer.stop()
+
+
+func _on_state_change(state: SpikeState):
+	match state:
+		SpikeState.HIDDEN:
+			sprite.play_backwards("default")
+			$DeadZone.disabled = true
+			timer.wait_time = active_time
+		SpikeState.ACTIVE:
+			sprite.play("default")
+			$DeadZone.disabled = false
+			timer.wait_time = hidden_time
+
+func _process(delta: float) -> void:
+	print(state)
+
+func _on_timer_timeout() -> void:
+	if state == SpikeState.HIDDEN:
+		state = SpikeState.ACTIVE
+	elif state == SpikeState.ACTIVE:
+		state = SpikeState.HIDDEN
