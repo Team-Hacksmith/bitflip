b13ab64b3e2bce81e331ca19831058b1fd015e8f
adityanandanx <nandanaditya985@gmail.com>
2025-02-03 05:30:42 +0530

    feat: music and dialogs

--- scripts/chapter3.gd
+++ scripts/chapter3.gd
@@ -4,3 +4,4 @@ func _ready() -> void:
 	Global.player_stats.add_part(0, false) # speed
 	Global.player_stats.add_part(1, false) # jump
 	Global.player.apply_abilities()
+	Dialogic.start("Chapter3Init")
